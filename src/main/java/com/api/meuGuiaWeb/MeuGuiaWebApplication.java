e11b4dfbef3ecbf80c6c673efca86745d4e8ad84
Galahad <gabrielbbaldez@outlook.com>
2023-09-04 21:00:48 -0300

    Version1

---
+++ src/main/java/com/api/meuGuiaWeb/MeuGuiaWebApplication.java
@@ -0,0 +1,13 @@
+package com.api.meuGuiaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuGuiaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuGuiaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/meuGuiaWeb/MeuGuiaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.meuGuiaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuGuiaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
